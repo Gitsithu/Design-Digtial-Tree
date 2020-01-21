92d3dec86cb188b64df6207ecf39740ce60d9ea8
Wai Yan Aung <waiyan.office@gmail.com>
2020-01-22 01:07:48 +0630

    edited pre loader for mobile view

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
 /*- - - - - - - - - - - - - - - - - - 
         Loading Section
 - - - - - - - - - - - - - - - - - -*/
- 
-  /*- - - - - - - - - - - - - - - - - - 
+
+/*- - - - - - - - - - - - - - - - - - 
         Scroll To Top
 - - - - - - - - - - - - - - - - - -*/
-$(document).ready(function(){
-$(window).scroll(function(){
-  if ($(this).scrollTop() > 100) {
-      $('.go-top').fadeIn();
-  } else {
-      $('.go-top').fadeOut();
-  }
-});
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.go-top').fadeIn();
+        } else {
+            $('.go-top').fadeOut();
+        }
+    });
 
-//Click event to scroll to top
-$('.go-top').click(function(){
-  $('html, body').animate({scrollTop : 0},800);
-  return false;
+    //Click event to scroll to top
+    $('.go-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
 });
+$(window).load(function() {
+    $("#loading").fadeOut(3500);
 });
-$(window).load(function(){
-  $("#loading").fadeOut(1500);
-});
-
-
